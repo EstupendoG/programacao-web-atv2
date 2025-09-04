ef1a2c4662150442320f21c91df7685d9eeae79c
unknown <rafagonpin317@gmail.com>
2025-09-04 18:45:32 -0300

    Header Finalizado

--- components/scripts/interface.js
+++ components/scripts/interface.js
@@ -4,14 +4,55 @@ const html = document.documentElement
 // =================================================== TEMA
 
 const iTheme = document.querySelector("#changeTheme")
+let currentTheme = 'dark'
 iTheme.addEventListener('click', changeTheme)
 
 function changeTheme() {
-    console.log('clicou ebaAAAAAA')
     iTheme.checked
-    ? html.setAttribute("data-theme", 'dark')
-    : html.setAttribute("data-theme", 'light')
+    ? currentTheme = 'dark'
+    : currentTheme = 'light'
+
+    html.setAttribute("data-theme", currentTheme)
+    changeIconsSet(currentTheme)
 }
 
-// =================================================== 
+// =================================================== ÍCONES
+// Preset
+function recreateIconSet(name){
+    let path = `assets/emojis/${name}/${name}`
+    let set = {
+        cellphone: `${path}-cellphone.png`,
+        smile: `${path}-smile.png`,
+    }
 
+    return set
+}
+// Setando
+let iconSet = {}
+function changeIconsSet(t){
+    let set = {}
+    if(t === 'dark') {
+        set = recreateIconSet('kuromi')
+        set.symbol = '#'
+    }
+    else
+    if(t === 'light') {
+        set = recreateIconSet('melody')
+        set.symbol = '@'
+    }
+
+    iconSet = set
+    
+    applyIcons()
+}
+
+// Aplicando
+function applyIcons(){
+    // Simbolo do Título # / @
+    document.querySelector('#titleSymbol').innerHTML = iconSet.symbol
+    // Ícone de Telefone
+    document.querySelector('#iconPhone').src = iconSet.cellphone
+    // Ícone da Seleção de Tema
+    document.querySelector('#iconSmile').src = iconSet.smile
+
+}
