f7d16ec95755641bc3312c3a8eb927dbea9d43ae
Diurno <rafagonpin317@gmail.com>
2025-09-08 10:42:08 -0300

    Form js

---
+++ components/scripts/form.js
@@ -0,0 +1,9 @@
+const iTaskName = document.querySelector('input#taskNome')
+const iTaskDate = document.querySelector('input#taskDate')
+const iTaskCategory = document.querySelectorAll("input[name='task_categoria']")
+const iTaskPriority = document.querySelectorAll("input[name='task_prioridade']")
+const iSubmitBtn = document.querySelector('button#submitBtn')
+
+const submitForm(){
+    
+}
